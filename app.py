aa3bcadb6a09246c63185f5fc33f3580dc5af287
Ostap Fedoriv <fedoriv-01@ukr.net>
2023-04-22 21:10:28 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template, redirect, url_for
+import spacy
+from spacy import displacy
+
+nlp = spacy.load("en_core_web_sm")
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route("/annotated", methods=["POST", "GET"])
+def annotated():
+    if request.method == "POST":
+        text = request.form["text-for-annotation"]
+        return text
+    else:
+        # for GET request
+        return render_template("annotated.html")
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    # Do something with the name
+    doc = nlp(name)
+    html = displacy.render(doc, style="ent", page=True)
+    return html
+    #return f'Hello, {doc.ents}!'
+
+if __name__ == '__main__':
+    app.run()
